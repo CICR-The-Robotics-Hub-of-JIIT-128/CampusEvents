1e9a3dd21519008f04c8c8b390739895340019b5
ankit0584 <sharmaas98ll@gmail.com>
2025-08-26 08:51:22 +0530

    OAuth implemented

---
+++ rbac-system/src/app/page.js
@@ -0,0 +1,22 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function HomePage() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Welcome, {session.user?.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>You are not signed in.</p>
+      <button onClick={() => signIn("google")}>Sign in with Google</button>
+    </div>
+  );
+}
